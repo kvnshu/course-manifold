2605ddc38c575f5e4bb4aef02a9f8a7eeb860d33
Kevin Xu <54183095+kvnshu@users.noreply.github.com>
2026-01-06 09:44:07 -0600

    update Vite config base path to match repository name

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/course-manifold/', // <--- Add this! (Must match your GitHub repo name exactly, with slashes)
+  base: '',
 })
